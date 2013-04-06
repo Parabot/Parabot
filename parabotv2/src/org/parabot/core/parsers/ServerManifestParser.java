7c3f47e335ecdb0e6828d77ed74b701866e838ff
Clisprail <clisprail@parabot.org>
2013-04-06 17:06:33 +0200

    Parser now checks for dev mode

--- parabotv2/src/org/parabot/core/parsers/ServerManifestParser.java
+++ parabotv2/src/org/parabot/core/parsers/ServerManifestParser.java
@@ -1,12 +1,40 @@
 package org.parabot.core.parsers;
 
+import java.net.URL;
+
+import org.parabot.core.Core;
+import org.parabot.core.desc.ServerDescription;
+
 /**
  * 
  * @author Clisprail
  *
  */
 public class ServerManifestParser {
+	private URL url = null;
 	
+	public ServerManifestParser(final URL url) {
+		this.url = url;
+	}
+	
+	/**
+	 * Gets server descriptions
+	 * @return list of descriptions
+	 */
+	public ServerDescription[] getDescriptions() {
+		if(Core.isDevMode()) {
+			return localDesc();
+		}
+		return publicDesc();
+	}
+
+	private ServerDescription[] publicDesc() {
+		return null;
+	}
+
+	private ServerDescription[] localDesc() {
+		return null;
+	}
 	
 
 }

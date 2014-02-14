b97c02278450b161a677d47eaaa8e22e6499515a
Jeroen Ketelaar <jeroenketelaar@me.com>
2014-02-14 03:40:50 +0100

    Better coding format

--- parabotv2/src/org/parabot/core/parsers/scripts/ScriptParser.java
+++ parabotv2/src/org/parabot/core/parsers/scripts/ScriptParser.java
@@ -10,49 +10,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 
  * Abstract class for parsing scripts
- * 
- * @author Everel
  *
+ * @author Everel
  */
 public abstract class ScriptParser {
-	
-	public static final Map<ScriptDescription, ScriptExecuter> SCRIPT_CACHE = new HashMap<ScriptDescription, ScriptExecuter>();
-	
-	public abstract void execute();
-	
-	public static ScriptDescription[] getDescriptions() {
-		SCRIPT_CACHE.clear();
-		final ArrayList<ScriptParser> parsers = new ArrayList<ScriptParser>();
-		if(Core.inLoadLocal()) {
-			parsers.add(new LocalJavaScripts());
-			if(Jython.isValid()) {
-				parsers.add(new LocalPythonScripts());
-			}
-            parsers.add(new SDNScripts());
-		}else if (Core.inDebugMode()){
+
+    public static final Map<ScriptDescription, ScriptExecuter> SCRIPT_CACHE = new HashMap<ScriptDescription, ScriptExecuter>();
+
+    public abstract void execute();
+
+    public static ScriptDescription[] getDescriptions() {
+        SCRIPT_CACHE.clear();
+        final ArrayList<ScriptParser> parsers = new ArrayList<ScriptParser>();
+        if (Core.inLoadLocal()) {
             parsers.add(new LocalJavaScripts());
-            if(Jython.isValid()) {
+            if (Jython.isValid()) {
                 parsers.add(new LocalPythonScripts());
             }
-        }else{
-			parsers.add(new SDNScripts());
-		}
-		
-		Core.verbose("Parsing scripts...");
-		for(final ScriptParser parser : parsers) {
-			parser.execute();
-		}
-		
-		if(Core.inVerboseMode()) {
-			for(final ScriptDescription desc : SCRIPT_CACHE.keySet()) {
-				Core.verbose(desc.toString());
-			}
-			Core.verbose("Scripts parsed.");
-		}
-		
-		return SCRIPT_CACHE.keySet().toArray(new ScriptDescription[SCRIPT_CACHE.size()]);
-	}
+            parsers.add(new SDNScripts());
+        } else if (Core.inDebugMode()) {
+            parsers.add(new LocalJavaScripts());
+            if (Jython.isValid()) {
+                parsers.add(new LocalPythonScripts());
+            }
+        } else {
+            parsers.add(new SDNScripts());
+        }
+
+        Core.verbose("Parsing scripts...");
+        for (final ScriptParser parser : parsers) {
+            parser.execute();
+        }
+
+        if (Core.inVerboseMode()) {
+            for (final ScriptDescription desc : SCRIPT_CACHE.keySet()) {
+                Core.verbose(desc.toString());
+            }
+            Core.verbose("Scripts parsed.");
+        }
+
+        return SCRIPT_CACHE.keySet().toArray(new ScriptDescription[SCRIPT_CACHE.size()]);
+    }
 
 }
--- parabotv2/src/org/parabot/core/parsers/servers/ServerParser.java
+++ parabotv2/src/org/parabot/core/parsers/servers/ServerParser.java
@@ -9,43 +9,41 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 
  * Abstract class for parsing server providers
- * 
- * @author Everel
  *
+ * @author Everel
  */
 public abstract class ServerParser {
-	public static final Map<ServerDescription, ServerExecuter> SERVER_CACHE = new HashMap<ServerDescription, ServerExecuter>();
-	
-	public abstract void execute();
-	
-	public static final ServerDescription[] getDescriptions() {
-		SERVER_CACHE.clear();
-		final ArrayList<ServerParser> parsers = new ArrayList<ServerParser>();
-		if(Core.inLoadLocal()) {
-			parsers.add(new LocalServers());
-            parsers.add(new PublicServers());
-		} else if (Core.inDebugMode()){
+    public static final Map<ServerDescription, ServerExecuter> SERVER_CACHE = new HashMap<ServerDescription, ServerExecuter>();
+
+    public abstract void execute();
+
+    public static final ServerDescription[] getDescriptions() {
+        SERVER_CACHE.clear();
+        final ArrayList<ServerParser> parsers = new ArrayList<ServerParser>();
+        if (Core.inLoadLocal()) {
             parsers.add(new LocalServers());
-        }else{
-			parsers.add(new PublicServers());
-		}
-		
-		Core.verbose("Parsing server providers...");
-		for(final ServerParser parser : parsers) {
-			parser.execute();
-		}
-		
-		if(Core.inVerboseMode()) {
-			for(final ServerDescription desc : SERVER_CACHE.keySet()) {
-				Core.verbose(desc.toString());
-			}
-			Core.verbose("Server providers parsed.");
-		}
-		
-		
-		return SERVER_CACHE.keySet().toArray(new ServerDescription[SERVER_CACHE.size()]);
-	}
+            parsers.add(new PublicServers());
+        } else if (Core.inDebugMode()) {
+            parsers.add(new LocalServers());
+        } else {
+            parsers.add(new PublicServers());
+        }
+
+        Core.verbose("Parsing server providers...");
+        for (final ServerParser parser : parsers) {
+            parser.execute();
+        }
+
+        if (Core.inVerboseMode()) {
+            for (final ServerDescription desc : SERVER_CACHE.keySet()) {
+                Core.verbose(desc.toString());
+            }
+            Core.verbose("Server providers parsed.");
+        }
+
+
+        return SERVER_CACHE.keySet().toArray(new ServerDescription[SERVER_CACHE.size()]);
+    }
 
 }

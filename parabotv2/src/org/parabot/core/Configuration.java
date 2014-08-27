43052ddd3fc2c6b088f522135fea5ed5d2f75543
JKetelaar <jeroen@ketelaar.me>
2014-08-27 02:58:01 +0200

    SSL for the main domain pages

--- parabotv2/src/org/parabot/core/Configuration.java
+++ parabotv2/src/org/parabot/core/Configuration.java
@@ -6,14 +6,14 @@ package org.parabot.core;
  * @author Everel
  */
 public class Configuration {
-    public static final String LOGIN_SERVER = "http://www.parabot.org/community/api/login.php?username=%s&password=%s";
+    public static final String LOGIN_SERVER = "https://www.parabot.org/community/api/login.php?username=%s&password=%s";
     public static final String SDN_SCRIPTS_JSON = "http://sdn.parabot.org/scripts.php?method=json&user=%s";
     public static final String GET_SDN_SCRIPT = "http://sdn.parabot.org/getscript.php?user=%s&pass=%s&scriptid=%d";
     public static final String GET_SERVER_PROVIDERS_JSON = "http://sdn.parabot.org/providers/index.php?method=json";
     public static final String GET_SERVER_PROVIDER = "http://sdn.parabot.org/providers/provider.php?id=";
     public static final String GET_SERVER_PROVIDER_INFO = "http://sdn.parabot.org/providers/getInformation.php?id=";
     public static final String GET_BOT_VERSION = "http://bot.parabot.org/version.txt";
-    public static final String REGISTRATION_PAGE = "http://www.parabot.org/community/index.php?app=core&module=global&section=register";
+    public static final String REGISTRATION_PAGE = "https://www.parabot.org/community/index.php?app=core&module=global&section=register";
 
     public static final double BOT_VERSION = 2.1;
 

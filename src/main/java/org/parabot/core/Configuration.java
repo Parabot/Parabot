17a4c28d391c1d6981bf23eeeb5495bfba26abfd
JKetelaar <jeroen@ketelaar.me>
2015-09-14 20:39:34 +0200

    Cleaned and changed configuration to correct URIs

--- src/main/java/org/parabot/core/Configuration.java
+++ src/main/java/org/parabot/core/Configuration.java
@@ -14,9 +14,8 @@ public class Configuration {
     public static final String GET_SERVER_PROVIDER = "http://bdn.parabot.org/api/get.php?action=server_provider&name=";
     public static final String GET_SERVER_PROVIDER_INFO = "http://bdn.parabot.org/api/get.php?action=server_information&name=";
     public static final String GET_BOT_VERSION = "http://bdn.parabot.org/api/v2/bot/version";
-    public static final String DOWNLOAD_BOT = "http://bdn.parabot.org/versions/";
-    public static final String REGISTRATION_PAGE = "https://www.parabot.org/community/index.php?app=core&module=global&section=register";
-    public static final String GET_PASSWORD = "http://bdn.parabot.org/api/get.php?action=password";
+    public static final String DOWNLOAD_BOT = "http://bdn.parabot.org/api/v2/bot/download/client/";
+    public static final String REGISTRATION_PAGE = "https://www.parabot.org/community/register/";
     public static final String GET_RANDOMS = "http://bdn.parabot.org/api/get.php?action=randoms";
     public static final String DATA_API = "http://bdn.parabot.org/api/v2/data/";
     public static final String ITEM_API = DATA_API + "items/";

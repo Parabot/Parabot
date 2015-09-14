9924a8bba9c88538a834f4a6dbee3629492c56da
JKetelaar <jeroen@ketelaar.me>
2015-09-14 13:49:21 +0200

    Updated POM

---
+++ deploy/deploy.bat
@@ -0,0 +1,4 @@
+mvn install:install-file -DgroupId=${project.groupId} -DartifactId=${project.artifactId} -Dversion=${project.version} -Dpackaging=jar -Dfile=../target/${project.build.finalName}.jar -DlocalRepositoryPath=../../Maven-Repository
+
+:: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.2.3 -Dpackaging=jar -Dfile=../target/Parabot-V2.2.3.jar -DlocalRepositoryPath=../../Maven-Repository
+:: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.2.32 -Dpackaging=jar -Dfile=../target/Parabot-V2.2.32.jar -DlocalRepositoryPath=../../Maven-Repository

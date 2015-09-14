12b0a469bb3ac280e538b2a204b8e755ae7b8734
JKetelaar <jeroen@ketelaar.me>
2015-09-14 21:52:30 +0200

    Added test to remove Ikov key

--- deploy/deploy.bat
+++ deploy/deploy.bat
@@ -4,3 +4,4 @@ mvn install:install-file -DgroupId=${project.groupId} -DartifactId=${project.art
 :: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.2.32 -Dpackaging=jar -Dfile=../target/Parabot-V2.2.32.jar -DlocalRepositoryPath=../../Maven-Repository
 :: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.2.33 -Dpackaging=jar -Dfile=../target/Parabot-V2.2.33.jar -DlocalRepositoryPath=../../Maven-Repository
 :: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.2.34 -Dpackaging=jar -Dfile=../target/Parabot-V2.2.34.jar -DlocalRepositoryPath=../../Maven-Repository
+:: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.2.35 -Dpackaging=jar -Dfile=../target/Parabot-V2.2.35.jar -DlocalRepositoryPath=../../Maven-Repository

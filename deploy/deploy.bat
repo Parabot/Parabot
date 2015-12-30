9806e4d7ffe9fed0815bcf664ade241eacc6edb3
JKetelaar <jeroen@ketelaar.me>
2015-12-30 17:51:46 +0100

    [CLEANUP] Removed previous release bat lines

--- deploy/deploy.bat
+++ deploy/deploy.bat
@@ -1,11 +1,3 @@
 mvn install:install-file -DgroupId=${project.groupId} -DartifactId=${project.artifactId} -Dversion=${project.version} -Dpackaging=jar -Dfile=../target/${project.build.finalName}-fat.jar -DlocalRepositoryPath=../../Maven-Repository
 
-:: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.2.3 -Dpackaging=jar -Dfile=../target/Parabot-V2.2.3.jar -DlocalRepositoryPath=../../Maven-Repository
-:: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.2.32 -Dpackaging=jar -Dfile=../target/Parabot-V2.2.32.jar -DlocalRepositoryPath=../../Maven-Repository
-:: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.2.33 -Dpackaging=jar -Dfile=../target/Parabot-V2.2.33.jar -DlocalRepositoryPath=../../Maven-Repository
-:: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.2.34 -Dpackaging=jar -Dfile=../target/Parabot-V2.2.34.jar -DlocalRepositoryPath=../../Maven-Repository
-:: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.2.35 -Dpackaging=jar -Dfile=../target/Parabot-V2.2.35.jar -DlocalRepositoryPath=../../Maven-Repository
-:: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.2.36 -Dpackaging=jar -Dfile=../target/Parabot-V2.2.36.jar -DlocalRepositoryPath=../../Maven-Repository
-:: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.2.4 -Dpackaging=jar -Dfile=../target/Parabot-V2.2.4.jar -DlocalRepositoryPath=../../Maven-Repository
-:: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.4 -Dpackaging=jar -Dfile=../target/Parabot-V2.4.jar -DlocalRepositoryPath=../../Maven-Repository
 :: mvn install:install-file -DgroupId=org.parabot -DartifactId=client -Dversion=2.4.1.1 -Dpackaging=jar -Dfile=../target/Parabot-V2.4.1.1-jar-with-dependencies.jar -DlocalRepositoryPath=../../Maven-Repository

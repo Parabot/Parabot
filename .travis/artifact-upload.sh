58bdd5cf8965b3c41638a04fbd863d38fd7bcb3b
JKetelaar <jeroen@ketelaar.me>
2016-03-10 01:52:52 +0100

    [BUGFIX] Got rid of the absolute path

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -2,7 +2,7 @@
  
 local_file="$(ls $TRAVIS_BUILD_DIR/target/final/Parabot-V*.jar | head -n 1)"
 local_path="$(ls $TRAVIS_BUILD_DIR/target/final/Parabot-V*.jar | head -n 1 | xargs -n 1 basename)"
-target_url='ftp://$FTP_HOST/~/public_html/uploads/version_control/$local_file'
+target_url='ftp://$FTP_HOST/$local_file'
  
 echo "Uploading $local_file to $target_url"
 curl -u $FTP_USERNAME:$FTP_PASSWORD -T "$local_path" "$target_url"

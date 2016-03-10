13bff21e1ae4f3d1f0279d137463fabe6062304a
JKetelaar <jeroen@ketelaar.me>
2016-03-10 01:11:13 +0100

    [FEATURE] Ability to upload builds

---
+++ .travis/artifact-upload.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+ 
+local_file="$(ls $TRAVIS_BUILD_DIR/target/final/Parabot-V*.jar | head -n 1)"
+local_path="$(ls $TRAVIS_BUILD_DIR/target/final/Parabot-V*.jar | head -n 1 | xargs -n 1 basename)"
+target_url='ftp://$FTP_HOST/~/uploads/version_control/$local_file'
+ 
+echo "Uploading $local_file to $target_url"
+curl -u $FTP_USER:$FTP_PASSWORD -T "$local_path" "$target_url"

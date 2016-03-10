72b1fcdc78da4d19ba9704b214abdd4a8395c25e
JKetelaar <jeroen@ketelaar.me>
2016-03-10 02:08:08 +0100

    [BUGFIX] Fixed an issue with the path location

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -2,7 +2,7 @@
  
 local_file="$(ls $TRAVIS_BUILD_DIR/target/final/Parabot-V*.jar | head -n 1 | xargs -n 1 basename)"
 local_path="$(ls $TRAVIS_BUILD_DIR/target/final/Parabot-V*.jar | head -n 1)"
-target_url='ftp://$FTP_HOST/$local_file'
+target_url="ftp://$FTP_HOST/$local_file"
  
 echo "Uploading $local_file to $target_url"
 curl -u $FTP_USERNAME:$FTP_PASSWORD -T "$local_path" "$target_url"

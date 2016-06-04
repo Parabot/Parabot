bbea2bb6d41454cb111e2e280705386003524e2e
JKetelaar <jeroen@ketelaar.me>
2016-06-04 15:35:44 +0200

    [FEATURE] Finished API Docs creation

--- .travis/call-creation.sh
+++ .travis/call-creation.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 sleep 15
-curl -sS "http://v3.bdn.parabot.org/api/bot/create/client?build_id=$TRAVIS_BUILD_ID&version=2.5.1" >/dev/null
+curl -sS "http://v3.bdn.parabot.org/api/bot/create/client?build_id=$TRAVIS_BUILD_ID&version=$PARABOT_VERSION" >/dev/null

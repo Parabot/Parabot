05abe6fc8adc242054fd38448458f5d26881ed2f
JKetelaar <jeroen@ketelaar.me>
2016-06-05 00:28:11 +0200

    [BUGFIX] Set randoms url to BDN V3

--- src/main/java/org/parabot/core/Configuration.java
+++ src/main/java/org/parabot/core/Configuration.java
@@ -8,7 +8,6 @@ import org.parabot.environment.api.utils.Version;
  * @author Everel
  */
 public class Configuration {
-//    public static final String LOGIN_SERVER = "https://www.parabot.org/community/api/login.php?username=%s&password=%s";
     public static final String LOGIN_SERVER = "http://bdn.parabot.org/api/v2/users/login";
     public static final String GET_SCRIPTS = "http://bdn.parabot.org/api/get.php?action=scripts_scripts&server=";
     public static final String GET_SCRIPT = "http://bdn.parabot.org/api/get.php?action=scripts_script&id=";
@@ -20,10 +19,9 @@ public class Configuration {
     public static final String API_DOWNLOAD_BOT = "http://bdn.parabot.org/api/v2/bot/download/client/";
     public static final String DOWNLOAD_BOT = "http://bdn.parabot.org/versions/";
     public static final String REGISTRATION_PAGE = "https://www.parabot.org/community/register/";
-    public static final String GET_RANDOMS = "http://bdn.parabot.org/api/get.php?action=randoms";
+    public static final String GET_RANDOMS = "http://v3.bdn.parabot.org/api/bot/download/randoms";
     public static final String DATA_API = "http://bdn.parabot.org/api/v2/data/";
     public static final String ITEM_API = DATA_API + "items/";
-    public static final String BUGSNAG_API = "d79752cf94dd4beb24c3d312a8609f53";
 
     public static final Version BOT_VERSION = ProjectProperties.getProjectVersion();
 

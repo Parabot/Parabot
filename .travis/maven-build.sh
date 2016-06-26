5d6b1086b95c506929e59c722f280bdc09d9b62e
JKetelaar <jeroen@ketelaar.me>
2016-06-26 17:38:03 +0200

    [CLEANUP] Removed zipping

--- .travis/maven-build.sh
+++ .travis/maven-build.sh
@@ -3,7 +3,6 @@
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     mvn -U package
     mkdir -p $TRAVIS_BUILD_DIR/target/apidocs/zips/
-    zip -r $TRAVIS_BUILD_DIR/target/apidocs/zips/$PARABOT_VERSION.zip $TRAVIS_BUILD_DIR/target/apidocs/$PARABOT_VERSION
 else
     mvn -Dbuild.version="-RC-$TRAVIS_BUILD_ID" -U package
 fi

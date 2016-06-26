c77f9bd81e861136e8e222fbc0e746914356f43c
JKetelaar <jeroen@ketelaar.me>
2016-06-26 17:38:52 +0200

    [CLEANUP] Removed command for directory creation

--- .travis/maven-build.sh
+++ .travis/maven-build.sh
@@ -2,7 +2,6 @@
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     mvn -U package
-    mkdir -p $TRAVIS_BUILD_DIR/target/apidocs/zips/
 else
     mvn -Dbuild.version="-RC-$TRAVIS_BUILD_ID" -U package
 fi

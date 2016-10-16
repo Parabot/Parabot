9fe519870913ad839710dca563e5a3f7e0ec868d
JKetelaar <jeroen@ketelaar.me>
2016-10-17 01:20:19 +0200

    [BUGFIX] We’re still in development, let’s show the content

--- .travis/call-creation.sh
+++ .travis/call-creation.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 sleep 5
-curl --data "build_id=$TRAVIS_BUILD_ID&version=$PARABOT_VERSION" -sS "http://v3.bdn.parabot.org/api/bot/create/client" >/dev/null
+curl --data "build_id=$TRAVIS_BUILD_ID&version=$PARABOT_VERSION" "http://v3.bdn.parabot.org/api/bot/create/client"

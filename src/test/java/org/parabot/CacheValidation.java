f77aa730eed27e26e580a60f8bf0d069259ac478
JKetelaar <jeroen@ketelaar.me>
2017-02-03 21:03:01 +0100

    [FEATURE] Added CacheValidation unit test

---
+++ src/test/java/org/parabot/CacheValidation.java
@@ -0,0 +1,34 @@
+package org.parabot;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.parabot.core.Directories;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author JKetelaar
+ */
+public class CacheValidation {
+
+    @Test
+    public void test(){
+        try {
+            File fileOne = new File(Directories.getCachePath(), "should-exist.tmp");
+            File fileTwo = new File(Directories.getCachePath(), "should-not-exist.tmp");
+
+            fileOne.createNewFile();
+            fileTwo.createNewFile();
+
+            fileTwo.setLastModified(System.currentTimeMillis() / 1000 - 350000);
+
+            Directories.clearCache(259200, false);
+
+            Assert.assertTrue(fileOne.exists());
+            Assert.assertTrue(!fileTwo.exists());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

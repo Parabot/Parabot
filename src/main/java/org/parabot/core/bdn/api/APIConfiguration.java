cec88d2a144b11c3f1a054fc23d315e3cb01e4e1
JKetelaar <jeroen@ketelaar.me>
2017-02-20 23:23:58 +0100

    [CLEANUP] Removed unused variable

--- src/main/java/org/parabot/core/bdn/api/APIConfiguration.java
+++ src/main/java/org/parabot/core/bdn/api/APIConfiguration.java
@@ -7,7 +7,6 @@ public class APIConfiguration {
 
     public static final String ENDPOINT = "http://v3.bdn.parabot.org/";
     public static final String API_ENDPOINT = ENDPOINT + "api/";
-    public static final String VALID_OAUTH_CHECK = API_ENDPOINT + "users/oauth/v2/valid";
     public static final String CLOSE_PAGE = ENDPOINT + "close";
     public static final String USERS_LOGIN = API_ENDPOINT + "users/log_in?after_login_redirect=%s";
     public static final String INTERNAL_ROUTE_CLIENT = ENDPOINT + "internal/route/oauth/v2/token/client";

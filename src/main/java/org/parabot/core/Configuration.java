839aa861c20aac0909bf5f6148ccfffe63206c71
JKetelaar <jeroen@ketelaar.me>
2017-02-21 21:39:08 +0100

    [CLEANUP] Removed unused download configuration

--- src/main/java/org/parabot/core/Configuration.java
+++ src/main/java/org/parabot/core/Configuration.java
@@ -8,7 +8,6 @@ import org.parabot.environment.api.utils.Version;
  * @author Everel
  */
 public class Configuration extends org.parabot.api.Configuration {
-    public static final String DOWNLOAD_BOT = "http://bdn.parabot.org/versions/";
     public static final String REGISTRATION_PAGE = "https://www.parabot.org/community/register/";
     public static final Version BOT_VERSION = ProjectProperties.getProjectVersion();
 }

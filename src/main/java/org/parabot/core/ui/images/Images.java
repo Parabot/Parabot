e3d73da31a01a7ede28ee12f917ab558da1572ac
JKetelaar <jeroen@ketelaar.me>
2017-02-21 22:11:54 +0100

    [CLEANUP] Removed unused classes

--- src/main/java/org/parabot/core/ui/images/Images.java
+++
@@ -1,9 +0,0 @@
-package org.parabot.core.ui.images;
-
-/**
- * Caches and loads images from resource
- *
- * @author Everel, JKetelaar
- */
-public final class Images extends org.parabot.api.io.images.Images {
-}

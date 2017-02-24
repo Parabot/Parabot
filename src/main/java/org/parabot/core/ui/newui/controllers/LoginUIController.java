e92eff58a3468e24444a19626709d23b06a3d385
JKetelaar <jeroen@ketelaar.me>
2017-02-24 22:00:52 +0100

    [TASK] Updated code to use Configuration data

--- src/main/java/org/parabot/core/ui/newui/controllers/LoginUIController.java
+++ src/main/java/org/parabot/core/ui/newui/controllers/LoginUIController.java
@@ -2,18 +2,36 @@ package org.parabot.core.ui.newui.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import org.parabot.core.settings.Configuration;
 import org.parabot.core.Core;
 import org.parabot.core.ui.newui.BotUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * @author Fryslan, JKetelaar
  */
-public class LoginUIController {
+public class LoginUIController implements Initializable {
 
     @FXML
     private AnchorPane login_panel;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private Label description;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.title.setText(Configuration.BOT_TITLE);
+        this.description.setText(Configuration.BOT_SLOGAN);
+    }
 
     @FXML
     private void login(ActionEvent event) {

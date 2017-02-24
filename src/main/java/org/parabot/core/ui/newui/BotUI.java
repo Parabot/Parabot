8b1f774aa7c46cdd62776f803fb965422b07deda
Fryslan <pietjetse@outlook.com>
2017-02-24 23:37:17 +0100

    [Update] Added minification icon.

--- src/main/java/org/parabot/core/ui/newui/BotUI.java
+++ src/main/java/org/parabot/core/ui/newui/BotUI.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import org.parabot.core.Core;
@@ -25,6 +26,7 @@ public class BotUI extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.initStyle(StageStyle.UNDECORATED);
+        stage.getIcons().add(new Image("/storage/images/icon.png"));
         switchState(ViewState.LOGIN, stage);
     }
 

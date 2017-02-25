ed51682cf34a3a5a7300961e7f74ad5aa9aa97df
JKetelaar <jeroen@ketelaar.me>
2017-02-25 12:45:40 +0100

    [TASK] Fixed typos

--- src/main/java/org/parabot/core/ui/newui/controllers/RegisterUIController.java
+++ src/main/java/org/parabot/core/ui/newui/controllers/RegisterUIController.java
@@ -1,12 +1,10 @@
 package org.parabot.core.ui.newui.controllers;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -18,21 +16,19 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
- * Created by Fryslan.
+ * @author Fryslan, JKetelaar
  */
-public class RegisterController implements Initializable {
+public class RegisterUIController implements Initializable {
     @FXML
-    private ImageView refreshIcon;
-
-    @FXML
-    private ImageView returnIcon;
-
+    private ImageView refreshIcon, returnIcon;
     @FXML
     private WebView registerWebView;
+    @FXML
+    private AnchorPane pane;
 
     @FXML
     private void returnToLogin(MouseEvent event) {
-        Stage stage = (Stage) returnIcon.getScene().getWindow();
+        Stage stage = (Stage) pane.getScene().getWindow();
         Core.getInjector().getInstance(BotUI.class).switchState(BotUI.ViewState.LOGIN, stage);
     }
 
@@ -45,15 +41,12 @@ public class RegisterController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         final WebEngine engine = registerWebView.getEngine();
         engine.load(Configuration.REGISTRATION_PAGE);
-        engine.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/601.6.17 (KHTML, like Gecko) Version/9.1.1 Safari/601.6.17");
+        engine.setUserAgent(Configuration.USER_AGENT);
         engine.getLoadWorker().stateProperty().addListener(
-                new ChangeListener<Worker.State>() {
-                    @Override
-                    public void changed(ObservableValue ov, Worker.State oldState, Worker.State newState) {
-                        if (engine.getLocation().endsWith("https://www.parabot.org/community//applications/core/interface/ipsconnect/ipsconnect.php&returnTo=https://www.parabot.org/community/")) {
-                            Stage stage = (Stage) registerWebView.getScene().getWindow();
-                            Core.getInjector().getInstance(BotUI.class).switchState(BotUI.ViewState.REGISTER_SUCCES, stage);
-                        }
+                (ov, oldState, newState) -> {
+                    if (engine.getLocation().contains("crossLogin")) {
+                        Stage stage = (Stage) pane.getScene().getWindow();
+                        Core.getInjector().getInstance(BotUI.class).switchState(BotUI.ViewState.REGISTER_SUCCESS, stage);
                     }
                 });
     }

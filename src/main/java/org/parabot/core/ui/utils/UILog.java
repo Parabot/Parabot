a22c20a7916ed07eacb447f0ed26fa783e84e89e
JKetelaar <jeroen@ketelaar.me>
2017-03-02 01:13:10 +0100

    [TASK] Deprecated UILog

--- src/main/java/org/parabot/core/ui/utils/UILog.java
+++ src/main/java/org/parabot/core/ui/utils/UILog.java
@@ -1,11 +1,15 @@
 package org.parabot.core.ui.utils;
 
+import javafx.scene.control.Alert;
+import org.parabot.core.ui.newui.components.DialogHelper;
+
 import javax.swing.*;
 
 /**
  * Log messages to the log user interface which is attached to the bot user interface
  *
- * @author Everel
+ * @author Everel, JKetelaar
+ * @deprecated Use DialogHelper instead
  */
 public class UILog {
 
@@ -15,8 +19,21 @@ public class UILog {
 
     public static void log(final String title, final String message,
                            int messageType) {
-        JOptionPane.showMessageDialog(null, message, title,
-                messageType);
+        Alert.AlertType type;
+        switch (messageType) {
+            case JOptionPane.ERROR_MESSAGE:
+                type = Alert.AlertType.ERROR;
+                break;
+            case JOptionPane.WARNING_MESSAGE:
+                type = Alert.AlertType.WARNING;
+                break;
+            case JOptionPane.INFORMATION_MESSAGE:
+            default:
+                type = Alert.AlertType.INFORMATION;
+                break;
+        }
+
+        DialogHelper.showMessage("Parabot", title, message, type);
     }
 
 }

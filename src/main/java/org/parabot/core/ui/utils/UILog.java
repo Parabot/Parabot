d6547e3b84dc7975b74947f2fdf054219004956c
Emma Stone <capslock_donater@hotmail.com>
2017-05-31 03:15:43 +0100

    [FEATURE] Alert of new version
    Related to issue #195

--- src/main/java/org/parabot/core/ui/utils/UILog.java
+++ src/main/java/org/parabot/core/ui/utils/UILog.java
@@ -1,24 +1,33 @@
 package org.parabot.core.ui.utils;
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 /**
- * 
  * Log messages to the log user interface which is attached to the bot user interface
- * 
- * @author Everel
  *
+ * @author Everel
  */
 public class UILog {
 
-	public static void log(final String title, final String message) {
-		log(title, message, JOptionPane.INFORMATION_MESSAGE);
-	}
+    public static void log(final String title, final String message) {
+        log(title, message, JOptionPane.INFORMATION_MESSAGE);
+    }
 
-	public static void log(final String title, final String message,
-			int messageType) {
-		JOptionPane.showMessageDialog(null, message, title,
-				messageType);
-	}
+    public static void log(final String title, final String message,
+                           int messageType) {
+        JOptionPane.showMessageDialog(null, message, title,
+                messageType);
+    }
 
+    public static int alert(final String title, final String message) {
+        alert(title, message, JOptionPane.YES_NO_OPTION);
+    }
+
+    public static int alert(final String title, final String message, int option) {
+        alert(title, message, option, JOptionPane.DEFAULT_OPTION);
+    }
+
+    public static int alert(final String title, final String message, int optionType, int messageType) {
+        JOptionPane.showConfirmDialog(null, message, title, optionType, messageType);
+    }
 }

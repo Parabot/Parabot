3f0d8d64feb64ea47b4ea5658d09c2c14f3baea4
Jeroen Ketelaar <jeroen@ketelaar.me>
2018-01-03 11:05:00 +0100

    [TASK] Added unit tests for the Timer class

--- src/main/java/org/parabot/environment/api/utils/Timer.java
+++ src/main/java/org/parabot/environment/api/utils/Timer.java
@@ -21,6 +21,16 @@ public class Timer {
 
     /**
      * Timer Constructor
+     *
+     * @param end
+     */
+    public Timer(long end, long start) {
+        this.start = start;
+        this.end = System.currentTimeMillis() + end;
+    }
+
+    /**
+     * Timer Constructor
      */
     public Timer() {
         this(0);

c754cd2ef06a8f111bcf32d905a54c3cff674e04
George Farrell <PBLord@users.noreply.github.com>
2018-05-20 23:36:53 +0100

    Update Context.java

--- src/main/java/org/parabot/core/Context.java
+++ src/main/java/org/parabot/core/Context.java
@@ -72,6 +72,13 @@ public class Context {
         this.defaultErr = System.err;
     }
 
+    public static double getJavaVersion() {
+        String version = System.getProperty("java.version");
+        int pos = version.indexOf('.');
+        pos = version.indexOf('.', pos+1);
+        return Double.parseDouble (version.substring (0, pos));
+    }
+
     public static Context getInstance(ServerProvider serverProvider) {
         return instance == null ? instance = new Context(serverProvider) : instance;
     }

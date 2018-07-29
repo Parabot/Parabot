097a0bdf7b5ed60ab26fa380c5f48ba45d18d666
Jak <shadow_jak@hotmail.co.uk>
2018-07-29 14:49:01 +0100

    Fix ServerDesc comparitor to allow duplicate server names

--- src/main/java/org/parabot/core/desc/ServerDescription.java
+++ src/main/java/org/parabot/core/desc/ServerDescription.java
@@ -37,6 +37,8 @@ public class ServerDescription implements Comparable<ServerDescription> {
 
     @Override
     public int compareTo(ServerDescription o) {
+        if (this.getServerName().equalsIgnoreCase(o.getServerName()))
+            return 1;
         return this.getServerName().compareTo(o.getServerName());
     }
 

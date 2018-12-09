aed8cd012bb3558c9fafbd6684b8972daeb17e20
Shadowrs <shadow_jak@hotmail.co.uk>
2018-12-09 12:26:24 +0000

    Replace JavaFX-Application based UIs with FX-embedded-in-swing

--- src/main/java/org/parabot/core/ui/components/notifications/NotificationUI.java
+++ src/main/java/org/parabot/core/ui/components/notifications/NotificationUI.java
@@ -1,20 +1,48 @@
 package org.parabot.core.ui.components.notifications;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-import org.parabot.core.Configuration;
+import org.parabot.api.output.Verboser;
+import org.parabot.api.ui.JavaFxUtil;
 
-public class NotificationUI extends Application {
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * A JavaFX Panel embedded into a Swing JFrame - handles notification settings
+ *
+ * @author Shadowrs
+ */
+public class NotificationUI extends JavaFxUtil {
+
+    final NotificationUI n;
+
+    private NotificationUI() {
+        super("/storage/ui/notifications.fxml", NotificationUIController.class);
+        this.n = this;
+    }
+
+    public static void create() {
+        new NotificationUI();
+    }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        //noinspection RedundantCast
-        BorderPane root = (BorderPane) FXMLLoader.load(this.getClass().getResource("/storage/ui/notifications.fxml"));
-        stage.setTitle(Configuration.BOT_TITLE);
-        stage.setScene(new Scene(root));
-        stage.show();
+    public WindowAdapter getWindowAdapter() {
+        return new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                Verboser.verbose("NotificationUI closed " + e);
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                // Anything here. JFrame hides on exit.
+                Verboser.verbose("NotificationUI closing " + e);
+                n.getFrame().dispose();
+            }
+        };
+    }
+
+    @Override
+    protected void onLaunched() {
+        n.getFrame().setTitle("Notifications");
     }
 }

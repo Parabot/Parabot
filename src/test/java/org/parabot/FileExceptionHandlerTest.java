6d0643cd3b7b49803de904c4c7a27d6e3144bc7c
Jeroen Ketelaar <jeroen@ketelaar.me>
2019-05-21 23:28:51 -0500

    [TASK] Adjusted exception handler unit test

--- src/test/java/org/parabot/FileExceptionHandlerTest.java
+++ src/test/java/org/parabot/FileExceptionHandlerTest.java
@@ -1,28 +1,39 @@
 package org.parabot;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.parabot.environment.handlers.exceptions.ExceptionHandler;
 import org.parabot.environment.handlers.exceptions.FileExceptionHandler;
 
-import java.io.File;
-
 public class FileExceptionHandlerTest {
 
     @Test
-    public void test() {
-        FileExceptionHandler serverHandler = new FileExceptionHandler(ExceptionHandler.ExceptionType.SERVER);
-        serverHandler.setIgnored(true);
+    public void manualTest() {
+        FileExceptionHandler handler = new FileExceptionHandler(ExceptionHandler.ExceptionType.CLIENT);
+        handler.setIgnored(true);
 
-        File[] reports = serverHandler.getReportsDirectory().listFiles();
-        int reportCount = 0;
-        if (reports != null) {
-            reportCount = reports.length;
+        Exception exception = new NullPointerException("Manual test");
+        handler.handle(exception);
+    }
+
+    @Test
+    public void threadHandlerTest() {
+        FileExceptionHandler handler = new FileExceptionHandler(ExceptionHandler.ExceptionType.CLIENT);
+        handler.setIgnored(true);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() throws NullPointerException {
+                throw new NullPointerException("Thread test");
+            }
+        };
+
+        thread.setUncaughtExceptionHandler(handler);
+        thread.start();
+
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-        Exception exception = new Exception("Test");
-        serverHandler.handle(exception);
-
-        Assert.assertTrue(serverHandler.getReportsDirectory().listFiles().length > reportCount);
     }
 }

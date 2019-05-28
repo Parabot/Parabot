38a49b3886fb44ffa2fedf6c9d339ba229612c83
Ethan <ethan.ducky@gmail.com>
2019-05-28 05:01:32 -0500

    Parabot - dark

---
+++ src/main/java/org/parabot/core/ui/themes/SubstanceDark.java
@@ -0,0 +1,10 @@
+package org.parabot.core.ui.themes;
+
+import org.pushingpixels.substance.api.SubstanceLookAndFeel;
+
+public final class SubstanceDark extends SubstanceLookAndFeel {
+
+    public SubstanceDark() {
+        super(new DarkTheme());
+    }
+}
